b18136a7916efaf5d30842328ac0ee031b1d4174
kaialiani <kaialiani01@gmail.com>
2023-03-26 22:49:45 +0200

    first commit

---
+++ script_animation.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+import { animate, stagger } from "https://cdn.skypack.dev/motion";
+
+
+animate(
+ ".header_graphics",
+ { y: -50 },
+ { duration: 1, delay: stagger(0.2), repeat: Infinity, direction: "alternate" }
+);
+
+animate(
+    ".product_graphics",
+    { rotate: -50 },
+    { duration: 1, delay: stagger(0.4), repeat: Infinity, direction: "alternate" }
+   );
