ce07df853de256c70a5461e41e4bca3bb30b25a8
kaialiani <111850398+kaialiani@users.noreply.github.com>
2023-04-17 00:49:17 +0200

    added logo + changes to buttons

--- script_animation.js
+++ script_animation.js
@@ -1,17 +1,15 @@
 "use strict";
 
-
 import { animate, stagger } from "https://cdn.skypack.dev/motion";
 
-
 animate(
- ".header_graphics",
- { y: -50 },
- { duration: 1, delay: stagger(0.2), repeat: Infinity, direction: "alternate" }
+  ".header_graphics",
+  { y: -50 },
+  { duration: 1, delay: stagger(0.2), repeat: Infinity, direction: "alternate" }
 );
 
 animate(
-    ".product_graphics",
-    { rotate: -50 },
-    { duration: 1, delay: stagger(0.4), repeat: Infinity, direction: "alternate" }
-   );
+  ".product_graphics",
+  { rotate: -50 },
+  { duration: 1, delay: stagger(0.4), repeat: Infinity, direction: "alternate" }
+);
