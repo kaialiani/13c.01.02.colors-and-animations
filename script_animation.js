d72c9eedbf37ed4c85ca21afd3c7c88eb18c0596
Kai Aliani <kaialiani01@gmail.com>
2023-05-22 18:12:09 +0200

    fixed styling

--- script_animation.js
+++ script_animation.js
@@ -3,7 +3,7 @@
 import { animate, stagger } from "https://cdn.skypack.dev/motion";
 
 animate(
-  ".header_graphics",
+  ".hero_graphics",
   { y: -50 },
   { duration: 1, delay: stagger(0.2), repeat: Infinity, direction: "alternate" }
 );
